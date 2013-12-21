5b2ae541fffd1905d1d33ecf6dd1587518b0b7a5
Daniel Schwen <daniel@schwen.de>
2013-12-21 11:17:50 -0700

    simpler and waaay faster loop checking

--- fastcci_circulartest.cc
+++ fastcci_circulartest.cc
@@ -15,44 +15,41 @@ int readFile(const char *fname, int* &buf) {
 
 const int maxdepth=300;
 int history[maxdepth];
+int subcatcount;
 int *cat; 
 int *tree; 
 char *mask;
 
 void tagCat(int id, int depth) {
-  // previously detected loop category
-  if (mask[id] == 2) return;
-
   // record path
   if (depth==maxdepth) return;
   history[depth] = id;
 
-  // previously visited during this traversal
-  if (mask[id] == 1) {
-    int i;
-
-    // output and mark entire loop 
-    for (i=0; i<=depth && history[i]!=history[depth]; ++i);
+  // previously visited category
+  int i;
+  if (mask[id] != 0) {
+    // is it a loop (check history)
+    for (i=0; i<depth; ++i) {
+      if (history[i] == id) break;
+    }
+    if (i==depth) return; // not a loop
+     printf("%d: ",history[0]);
     for (; i<=depth; ++i) {
       printf("%d|", history[i]);
-      mask[history[i]]=2;
     }
-
     printf("\n");
     return;
   }
 
   // mark as visited
   mask[id]=1;
+  subcatcount++;
   int c = cat[id], cend = tree[c];
   c += 2;
   while (c<cend) {
     tagCat(tree[c], depth+1);
     c++; 
   }
-
-  // back-track
-  if (mask[id]==1) mask[id]=0;
 }
 
 int main() {
@@ -62,10 +59,14 @@ int main() {
 
   readFile("fastcci.tree", tree);
 
+  int maxsubcatcount = 0;
   memset(mask,0,maxcat);
   for (int i=0; i<maxcat; ++i) {
     if (cat[i]>-1 && mask[i]==0) {
+      subcatcount = 0;
       tagCat(i,0); 
+      /*if (subcatcount > maxsubcatcount) {
+      }*/
     }
   }
 
