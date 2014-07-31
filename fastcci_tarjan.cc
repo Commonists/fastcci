c4a69ca4e2bb0d762650e7ea7f510b47d4fbcff3
Daniel Schwen <daniel@schwen.de>
2014-07-31 17:12:15 +0000

    fix tajan stuff, better output

--- fastcci_tarjan.cc
+++ fastcci_tarjan.cc
@@ -24,32 +24,34 @@ int *id;
 int *lowlink;
 
 // current id value
-int index = 1;
+int curr_id = 1;
 
 // the stack
 int s = 0;
 int *S;
 char *Smask;
 
+int *scc;
+
 void strongconnect(int v) {
   int w;
 
   // Set the depth index for v to the smallest unused index
-  id[v] = lowlink[v] = index++;
+  id[v] = lowlink[v] = curr_id++;
 
   // push to the stack
   S[s++] = v;
   Smask[v]++;
 
   // Consider successors of v
-  int c = cat[id], cend = tree[c];
+  int c = cat[v], cend = tree[c];
   c += 2;
   while (c<cend) {
     w = tree[c];
     if (id[w] == 0) 
     {
       // Successor w has not yet been visited; recurse on it
-      strongconnect(w)
+      strongconnect(w);
       lowlink[v] = lowlink[w] < lowlink[v] ? lowlink[w] : lowlink[v]; 
     } 
     else if (Smask[w])
@@ -62,18 +64,26 @@ void strongconnect(int v) {
   }
 
   // If v is a root node, pop the stack and generate an SCC
-  if (lowlink[v] = id[v])
+  if (lowlink[v] == id[v])
   {
     // start a new strongly connected component
+    int n = 0;
     do {
       // pop from stack
       w = S[--s]; 
       Smask[w]--;
 
       //add w to current strongly connected component
-      printf("%d ", w);
-    } while  (w != v)
-    printf("\n");
+      scc[n++] = w;
+    } while  (w != v);
+
+    // do not print single node SCCs
+    if (n>1) {
+      printf("%d : ", n);
+      for (int i=0; i<n; ++i)
+        printf(i>0 ? "|%d" : "%d", scc[i]);
+      printf("\n");
+    }
   }
 }
 
@@ -81,19 +91,22 @@ int main() {
   int maxcat = readFile("../fastcci.cat", cat);
   maxcat /= sizeof(int);
   
-  id      = (int*)malloc(maxcat);
-  lowlink = (int*)malloc(maxcat);
+  lowlink = (int*)malloc(maxcat * sizeof(*lowlink));
+  id      = (int*)malloc(maxcat * sizeof(*id));
   memset(id, 0, maxcat * sizeof(*id));
 
-  S = (int*)malloc(maxcat);
+  S = (int*)malloc(maxcat * sizeof(*S));
   Smask = (char*)malloc(maxcat);
   memset(Smask, 0, maxcat);
+
+  // scc result buffer
+  scc = (int*)malloc(maxcat * sizeof(*scc));
 
   readFile("../fastcci.tree", tree);
 
   for (int v=0; v<maxcat; ++v)
     if (cat[v]>-1 && id[v]==0) 
-      strongconnect(i); 
+      strongconnect(v); 
 
   free(cat);
   free(tree);
