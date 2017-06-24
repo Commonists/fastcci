2700d9c0aedd98b97fb8426596d4d1629fb44bf2
Daniel Schwen <daniel@schwen.de>
2017-06-24 15:52:24 +0000

    cleanup script

--- assets/update_database.sh
+++ assets/update_database.sh
@@ -2,5 +2,4 @@
 cd $HOME/data
 
 query="select /* SLOW_OK */ cl_from, page_id, cl_type from categorylinks,page where cl_type!=\"page\" and page_namespace=14 and page_title=cl_to order by page_id;"
-#mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent > sqldump && fastcci_build_db < sqldump
 mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent | fastcci_build_db
