72ac454ac2b6acaa59070e564f1920ef044f3e20
Daniel Schwen <daniel@schwen.de>
2017-06-24 21:05:22 +0000

    install ssh key

--- assets/setup_fastcci_worker_instance.sh
+++ assets/setup_fastcci_worker_instance.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+# install prerequisites
 sudo apt-get -y install make cmake gcc g++
 
-# get worker number from hostname
-WORKER=$(hostname | perl -lpe 's/.*[^\d](\d+)$/\1/g')
-
+# build onion http and websocket library
 cd $HOME/onion
 mkdir -p mybuild
 cd mybuild
@@ -13,6 +12,7 @@ cmake ..
 make
 sudo make install
 
+# build the fastcci server and database builder
 cd $HOME/fastcci
 mkdir -p mybuild
 cd mybuild
@@ -21,6 +21,7 @@ cmake ..
 make
 sudo make install
 
+# register fastcci server with systemd
 cd $HOME/fastcci
 sudo cp assets/fastcci.service /lib/systemd/system/fastcci.service
 cd /etc/systemd/system
@@ -28,5 +29,9 @@ sudo ln -sf /lib/systemd/system/fastcci.service .
 sudo systemctl daemon-reload
 sudo systemctl start fastcci.service
 
+# add cron entry for database update
 cd $HOME
 (crontab -l ; echo "*/1 * * * * $HOME/bin/restart_fastcci.sh")| crontab -
+
+# make sure master derver can push updated DBs
+sudo cp $HOME/.ssh/id_rsa.pub /etc/ssh/userkeys/dschwen
