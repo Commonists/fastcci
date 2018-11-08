5a3f49c83b3d856e0b62c3defe6f1931cb089d5d
Daniel Schwen <daniel@schwen.de>
2018-11-08 22:03:00 +0000

    log db update success or failure

--- assets/update_database.sh
+++ assets/update_database.sh
@@ -6,7 +6,7 @@ cd /tmp
 query="select /* SLOW_OK */ cl_from, page_id, cl_type from categorylinks,page where cl_type!=\"page\" and page_namespace=14 and page_title=cl_to order by page_id;"
 
 set -o pipefail
-mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent | fastcci_build_db || exit
+mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent | fastcci_build_db || ( date >> $HOME/update_fail.log; exit )
 
 for server in 1 2 
 do
@@ -20,3 +20,5 @@ do
   # sleep two minutes to have staggered updates in the cluster
   sleep 120
 done
+
+date >> $HOME/update_success.log
