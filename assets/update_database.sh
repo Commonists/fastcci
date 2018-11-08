ad53a695bce9ca7cc74f2ab3b96445aa95510cf3
Daniel Schwen <daniel@schwen.de>
2018-11-08 21:05:51 +0000

    use pipefail to detect DB crap out

--- assets/update_database.sh
+++ assets/update_database.sh
@@ -4,7 +4,9 @@
 cd /tmp
 
 query="select /* SLOW_OK */ cl_from, page_id, cl_type from categorylinks,page where cl_type!=\"page\" and page_namespace=14 and page_title=cl_to order by page_id;"
-mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent | fastcci_build_db
+
+set -o pipefail
+mysql --defaults-file=$HOME/replica.my.cnf -h commonswiki.labsdb commonswiki_p -e "$query" --quick --batch --silent | fastcci_build_db || exit
 
 for server in 1 2 
 do
