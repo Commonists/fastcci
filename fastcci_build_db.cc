badcdf5408616ffe7bb64c94150f6bbb37992a7c
Daniel Schwen <daniel@schwen.de>
2022-01-11 20:29:13 -0700

    Use mmap

--- fastcci_build_db.cc
+++ fastcci_build_db.cc
@@ -1,19 +1,33 @@
 #include "fastcci.h"
+#include "errno.h"
 
+int fd_cat, fd_tree;
 int maxtree = 1024*128, maxcat = 1024*128;
 tree_type *cat=NULL, *tree=NULL;
 
 void growTree(int max=0) {
-  if (tree==NULL)
-    tree = (tree_type*)malloc(maxtree * sizeof *tree);
-  else {
+  if (tree == NULL) {
+    if (ftruncate(fd_tree, maxtree * sizeof *tree))
+      printf("fruncate errno=%d fd=%d\n", errno, fd_tree);
+
+    tree = (tree_type*)mmap(NULL, maxtree * sizeof *tree, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_NORESERVE, fd_tree, 0);
+  } else {
+    int old_maxtree = maxtree;
     maxtree *= 2;
     if (maxtree<=max) maxtree = max+1;
-    tree = (tree_type*)realloc(tree, maxtree * sizeof *tree);
-  }
+    
+    if (ftruncate(fd_tree, maxtree * sizeof *tree))
+      printf("errno = %d\n", errno);
 
+    tree = (tree_type*)mremap(tree, old_maxtree * sizeof *tree,  maxtree * sizeof *tree, MREMAP_MAYMOVE);
+  }
+  
   // check for allocation error
-  if (tree==NULL) {
+  if (tree == MAP_FAILED) {
+    perror("growTree() MAP_FAILED");
+    exit(1);
+  }
+  if (tree == NULL) {
     perror("growTree()");
     exit(1);
   }
@@ -22,20 +36,28 @@ void growTree(int max=0) {
 }
 
 void growCat(int max=0) {
-  int a;
-  if (cat==NULL) {
-    cat = (tree_type*)malloc(maxcat * sizeof *cat);
-    a = 0;
+  int a = 0;
+
+  if (cat == NULL) {
+    if (ftruncate(fd_cat, maxcat * sizeof *cat))
+      printf("fruncate errno=%d fd=%d\n", errno, fd_cat);
+
+    cat = (tree_type*)mmap(NULL, maxcat * sizeof *cat, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_NORESERVE, fd_cat, 0);
   } else {
     a = maxcat;
     maxcat *= 2;
     if (maxcat<=max) maxcat = max+1;
-    cat = (tree_type*)realloc(cat, maxcat * sizeof *cat);
+    ftruncate(fd_cat, maxcat * sizeof *cat);
+    cat = (tree_type*)mremap(cat, a * sizeof *cat,  maxcat * sizeof *cat, MREMAP_MAYMOVE);
   }
 
   // check for allocation error
-  if (cat==NULL) {
-    perror("growCat()");
+  if (cat == MAP_FAILED) {
+    perror("growCat() MAP_FAILED");
+    exit(1);
+  }
+  if (cat == NULL) {
+    perror("growCat() NULL");
     exit(1);
   }
 
@@ -49,10 +71,19 @@ void growCat(int max=0) {
 int main(int argc, char *argv[]) {
   int i, j;
 
-  // category index
-  growCat();
+  // file descriptors for mmap
+  fd_cat = open("fastcci.cat", O_RDWR|O_CREAT, 0744);
+  if (fd_cat == -1) {
+    printf("Error opening cat file: %d\n", errno);
+    return 1;
+  }
+  fd_tree = open("fastcci.tree", O_RDWR|O_CREAT, 0744);
+  if (fd_tree == -1) {
+    printf("Error opening tree file: %d\n", errno);
+    return errno;
+  }
 
-  // category tree buffer
+  growCat();
   growTree();
 
   // insert empty dummy category at tree[0]
@@ -159,15 +190,12 @@ int main(int argc, char *argv[]) {
   }
 
   // write out binary tree files
-  printf("writing db files...\n");
-  FILE *outtree = fopen("fastcci.tree","wb");
-  FILE *outcat  = fopen("fastcci.cat","wb");
-
-  fwrite(tree, sizeof *tree, cfile,    outtree);
-  fwrite(cat,  sizeof *cat,  lcl_to+1, outcat);
-
-  fclose(outtree);
-  fclose(outcat);
+  munmap(cat, maxtree * sizeof *cat);
+  munmap(tree, maxtree * sizeof *tree);
+  ftruncate(fd_tree, cfile * sizeof *tree);
+  ftruncate(fd_cat, (lcl_to+1) * sizeof *cat);
+  close(fd_tree);
+  close(fd_cat);
 
   printf("db files written.\n");
 
@@ -180,9 +208,6 @@ int main(int argc, char *argv[]) {
   }
   else
     printf("Could not open file.\n");
-
-  free(tree);
-  free(cat);
 
   return 0;
 }
